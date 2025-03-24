8c99e8ec4344c440247dad1555888d5818af77da
Brandon Johnson <johnsbr99@gmail.com>
2025-03-24 06:44:03 -0600

    Added header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
-export default function App() {
+import React from "react"
+
+/**
+ * Goal: Build out the main parts of our app
+ * 
+ * Challenge: Add a header with the game title
+ * and description. Startin' out easy 🙂🚶‍➡️
+ */
+
+export default function AssemblyEndgame() {
   return (
-    <h1>Hello World!</h1>
+    <main>
+      <header>
+          <h1>Assembly: Endgame</h1>
+          <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
+      </header>
+    </main>
+
   )
 }
