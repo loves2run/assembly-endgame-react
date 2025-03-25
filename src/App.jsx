b98b3abada989ffb55c66a0cdf260125a52fafa5
Brandon Johnson <johnsbr99@gmail.com>
2025-03-25 06:51:59 -0600

    Added static status bar for win

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import React from "react"
 /**
  * Goal: Build out the main parts of our app
  * 
- * Challenge: Add a header with the game title
- * and description. Startin' out easy 🙂🚶‍➡️
+ * Challenge: Build a status section below the header.
+ * For now, you can just hard-code in the styles for
+ * a winning game, and we'll make it more dynamic
+ * later.
  */
 
 export default function AssemblyEndgame() {
@@ -14,6 +16,10 @@ export default function AssemblyEndgame() {
           <h1>Assembly: Endgame</h1>
           <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
       </header>
+      <section id="statusBar">
+        <h2>You Win!</h2><br/>
+        <span>Well done! 🎉</span>
+      </section>
     </main>
 
   )
