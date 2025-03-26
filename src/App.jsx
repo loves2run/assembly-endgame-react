da27910cd8343544a728acf4a9ec434c5208d48e
Brandon Johnson <johnsbr99@gmail.com>
2025-03-26 07:46:52 -0600

    Dynamically rendered languages

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,48 @@
 import React from "react"
+// import Language from "./components/Language"
+import { languages } from "./languages"
+
 
 /**
  * Goal: Build out the main parts of our app
  * 
- * Challenge: Build a status section below the header.
- * For now, you can just hard-code in the styles for
- * a winning game, and we'll make it more dynamic
- * later.
+ * Challenge: Create the language chips. Use the
+ * `languages.js` file to pull in the array of
+ * languages to use, which contains the language
+ * name, background color, and text color.
+ * 
+ * Hint for layout: use a flex container that can wrap
+ * to layout the languages.
  */
 
 export default function AssemblyEndgame() {
+
+  const languageElements = languages.map((lang) => {
+    const styles = {
+      backgroundColor: lang.backgroundColor,
+      color: lang.color
+    }
+    return (
+      <span className="langChips" key={lang.name} style={styles}>
+        {lang.name}
+      </span>
+    )
+  })
+
   return (
     <main>
       <header>
           <h1>Assembly: Endgame</h1>
           <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
       </header>
+
       <section id="statusBar">
         <h2>You Win!</h2><br/>
         <span>Well done! 🎉</span>
+      </section>
+
+      <section className="languageChips">
+        {languageElements}
       </section>
     </main>
 
