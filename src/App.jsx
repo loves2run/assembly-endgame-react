65698572c95820930795c099e7eb65cd353f50e1
Brandon Johnson <johnsbr99@gmail.com>
2025-04-08 08:15:55 -0600

    Conditionally rendered status bar message

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,6 @@ import {useState} from 'react'
 import clsx from 'clsx'
 import { languages } from "./languages"
 
-/**
- * Goal: Add in the incorrect guesses mechanism to the game
- * 
- * Challenge:
- * 1. Create a variable `isGameOver` which evaluates to `true`
- *    if the user has guessed incorrectly 8 times. Consider how
- *    we might make this more dynamic if we were ever to add or
- *    remove languages from the languages array.
- * 2. Conditionally render the New Game button only if the game
- *    is over.
- */
 
 
 export default function AssemblyEndgame() {
@@ -22,13 +11,9 @@ export default function AssemblyEndgame() {
 
   //derived values
   const wrongGuessCount = guesses.filter(letter => !currentWord.includes(letter)).length
-  const isGameOver = wrongGuessCount >= languages.length -1 || guesses.length - wrongGuessCount >= currentWord.length
-
-  // console.log('isGameOver:', isGameOver)
-  // console.log('wrongGuessCount:', wrongGuessCount)
-  // console.log('languages:', languages.length -1)
-  // console.log('gueses', guesses.length)
-  // console.log('currentWord', currentWord.length)
+  const isGameWon = guesses.length - wrongGuessCount >= currentWord.length
+  const isGameLost = wrongGuessCount >= languages.length -1
+  const isGameOver = isGameLost || isGameWon
 
   //static values
   const alphabet = "abcdefghijklmnopqrstuvwxyz"
@@ -105,6 +90,43 @@ Bob showed us 2 methods
     )
   })
 
+  /**
+ * Goal: Add in the incorrect guesses mechanism to the game
+ * 
+ * Challenge:
+ * Conditionally render either the "won" or "lost" statuses
+ * from the design, both the text and the styles, based on the
+ * new derived variables.
+ * 
+ * Note: We always want the surrounding `section` to be rendered,
+ * so only change the content inside that section. Otherwise the
+ * content on the page would jump around a bit too much.
+ */
+
+  const RenderGameStatus = () => {
+
+    if (isGameWon) {
+      return (
+        <>
+          <h2>You Win!</h2><br/>
+          <span>Well done! 🎉</span>
+        </>
+      )
+    } else if (isGameLost) {
+      return (
+        <>
+          <h2>Game Over!</h2><br/>
+          <span>You lose! Better start learning Assembly 😭</span>
+        </>
+      )
+    } else {
+      return (<></>)
+    }
+  }
+
+
+
+
   return (
     <main>
       <header>
@@ -112,9 +134,8 @@ Bob showed us 2 methods
           <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
       </header>
 
-      <section id="statusBar">
-        <h2>You Win!</h2><br/>
-        <span>Well done! 🎉</span>
+      <section id="statusBar" className={clsx({'bgGameInProgress' : !isGameOver, 'bgGameWon': isGameWon, 'bgGameLost' : isGameLost})}>
+        <RenderGameStatus />
       </section>
 
       <section className="languageChips">
